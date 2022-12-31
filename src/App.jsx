d5908fd287fe8afefad664e3ae0c17decea087e3
Vivek jha <vivekjha3080@gmail.com>
2022-12-31 23:03:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,44 @@ import React, { useEffect } from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import { About } from "./pages"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { useState } from "react"
 import Features from "./pages/Features"
-<<<<<<< HEAD
-=======
 import Dashboard from "./pages/Dashboard"
 import Sidebar from './components/Sidebar'
+import { useAuth0 } from "@auth0/auth0-react";
 
->>>>>>> 70b297cd0a75e4358250fba3aa50a8cc28c9df36
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const { isAuthenticated } = useAuth0();
+  const [loggedIn, useloggin] = useState(true);
 
-  return (
-
-    <div className="App">
+  if (!isAuthenticated) {
+    return (<div className="App">
       <Navbar></Navbar>
-        <div className="flex justify-start w-[100vw] m-0 p-0">
-        <div className=" ">
-        <Sidebar loggedIn={loggedIn}></Sidebar>
-        </div>
-      <div className="w-[100%]">
+      <div>
         <Routes>
-<<<<<<< HEAD
           <Route path='/' element={<Hero />}></Route>
           <Route path='/about' element={<About />}></Route>
           <Route path='/features' element={<Features />}></Route>
-=======
-        <Route path='/' element={<Hero/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/features' element={<Features/>}></Route>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
->>>>>>> 70b297cd0a75e4358250fba3aa50a8cc28c9df36
-
         </Routes>
-
-        </div>
+      </div>
+    </div>)
+  }
+  else {
+    return (<div className="App flex justify-start w-[100vw] m-0 p-0">
+      <Sidebar loggedIn={loggedIn}></Sidebar>
+      <div className="w-[100%]">
+        <Routes>
+          <Route path='/' element={<Dashboard />}></Route>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+        </Routes>
       </div>
 
-    </div>
-  )
+    </div>)
+
+  }
+
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Auth0Provider
       domain="dev-h8t8mfag6tjhntcr.us.auth0.com"
       clientId="tFeYafankxo8kjSbuVFo1sFWee3mQKpi"
-      redirectUri={'localhost:5173/dashboard'}
+      redirectUri={window.location.origin}
     >
 
       <BrowserRouter>
